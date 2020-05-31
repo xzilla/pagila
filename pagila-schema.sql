63af043767ed702c2e9261d2a7564560825415ee
Robert Treat <rob@xzilla.net>
2020-05-31 00:01:16 -0400

    use := for setting var value in last updated function
    This is more readable syntax than just using =, which can be confused for
    testing equality.

--- pagila-schema.sql
+++ pagila-schema.sql
@@ -222,7 +222,7 @@ CREATE FUNCTION public.last_updated() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
 BEGIN
-    NEW.last_update = CURRENT_TIMESTAMP;
+    NEW.last_update := CURRENT_TIMESTAMP;
     RETURN NEW;
 END $$;
 
